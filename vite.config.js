e5fd5e766a09b244b5ba7c28fd21ffd5bcc1d641
synan-mannan <syedmark786@gmail.com>
2026-01-13 13:24:42 +0530

    change 2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/Assignment_ServiceHive_Frontend/",
   server: {
     port: 5173,
     proxy: {
